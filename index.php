e12b48a0f167da7d80149b36db2dd4d9ba62706b
CloudSalander <ruben.as532@gmail.com>
2026-04-14 18:11:34 +0200

    Setting number of turns to generate

--- index.php
+++ index.php
@@ -2,9 +2,17 @@
 
 include('class/TurnManager.php');
 
+define('DEFAULT_TURNS_TO_GENERATE', 10);
+
 $turnManager = new TurnManager();
-$turnManager->generateTurn();
-$turnManager->generateTurn();
-$turnManager->generateTurn();
-$turnManager->generateTurn();
+
+$generatedTurns = 0;
+$turnToGenerate = $argv[1] ?? DEFAULT_TURNS_TO_GENERATE;  
+
+while($turns < $turnToGenerate) {
+    $turnManager->generateTurn();
+    ++$turns;
+}
+$turnManager->callTurn();
+
 ?>
